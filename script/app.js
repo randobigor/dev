f93bc2df6c0c39c48825e9bf7f61b3087353d501
randobigor <bodnar.igor.98@gmail.com>
2020-09-19 12:43:04 +0300

    1.3.8.1

--- script/app.js
+++ script/app.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
+    console.log('Page successfully loaded!')
     particlesJS.load('particles-js', 'assets/particles.json', function() {
         console.log('callback - particles.js config loaded');
     });
