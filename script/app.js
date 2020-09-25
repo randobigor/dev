5c7fd31678d0eda68d6339b4b4e057122da7bae3
randobigor <bodnar.igor.98@gmail.com>
2020-09-25 12:22:25 +0300

    Updated for phones & some blocks

--- script/app.js
+++ script/app.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('Page successfully loaded!')
     particlesJS.load('particles-js', 'assets/particles.json', function() {
         console.log('callback - particles.js config loaded');
     });
